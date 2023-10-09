834539b9879a9655270813e970253e48e300b919
KIMIOTA <116443510+KIMIOTA@users.noreply.github.com>
2023-10-09 19:11:10 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <cmath>
+
+class TQuadrangle {
+private:
+    double side1, side2, side3, side4;
+    double angle1, angle2, angle3, angle4;
+
+public:
+    TQuadrangle(double s1, double s2, double s3, double s4, double a1, double a2, double a3, double a4)
+        : side1(s1), side2(s2), side3(s3), side4(s4), angle1(a1), angle2(a2), angle3(a3), angle4(a4) {}
+
+    // Метод для визначення типу чотирикутника
+    std::string determineType() {
+        if (side1 == side2 && side2 == side3 && side3 == side4) {
+            if (angle1 == 90 && angle2 == 90 && angle3 == 90 && angle4 == 90)
+                return "Квадрат";
+            else
+                return "Ромб";
+        }
+        else {
+            if (angle1 == 90 && angle2 == 90 && angle3 == 90 && angle4 == 90)
+                return "Прямокутник";
+            else
+                return "Загальний чотирикутник";
+        }
+    }
+
+    // Метод для знаходження периметру чотирикутника
+    double findPerimeter() {
+        return side1 + side2 + side3 + side4;
+    }
+
+    // Метод для знаходження площі чотирикутника
+    double findArea() {
+        // Використовуємо формулу Герона для обчислення площі загального чотирикутника
+        double s = (side1 + side2 + side3 + side4) / 2;
+        double area = sqrt((s - side1) * (s - side2) * (s - side3) * (s - side4));
+        return area;
+    }
+};
+
+int main() {
+    double s1, s2, s3, s4, a1, a2, a3, a4;
+
+    std::cout << "Введіть довжини сторін та кути чотирикутника:" << std::endl;
+    std::cin >> s1 >> s2 >> s3 >> s4 >> a1 >> a2 >> a3 >> a4;
+
+    TQuadrangle quadrangle(s1, s2, s3, s4, a1, a2, a3, a4);
+
+    std::string type = quadrangle.determineType();
+    std::cout << "Тип чотирикутника: " << type << std::endl;
+
+    double perimeter = quadrangle.findPerimeter();
+    std::cout << "Периметр чотирикутника: " << perimeter << std::endl;
+
+    double area = quadrangle.findArea();
+    std::cout << "Площа чотирикутника: " << area << std::endl;
+
+    return 0;
+}
---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+
+class PlaneEquation {
+private:
+    double A, B, C, D;
+
+public:
+    PlaneEquation(double a, double b, double c, double d) : A(a), B(b), C(c), D(d) {}
+
+    // Індексатор для доступу до коефіцієнтів
+    double& operator[](int index) {
+        switch (index) {
+        case 0: return A;
+        case 1: return B;
+        case 2: return C;
+        case 3: return D;
+        default:
+            throw std::out_of_range("Індекс виходить за межі діапазону.");
+        }
+    }
+
+    // Метод для виведення рівняння площини
+    void display() {
+        std::cout << A << "x + " << B << "y + " << C << "z + " << D << " = 0" << std::endl;
+    }
+
+    // Метод для знаходження точки перетину площини з осями координат
+    // Результат - координати точки перетину (x, y, z)
+    void findIntersectionWithAxes() {
+        double x, y, z;
+
+        // Площина з осью X: x = -D / A
+        x = -D / A;
+
+        // Площина з осью Y: y = -D / B
+        y = -D / B;
+
+        // Площина з осью Z: z = -D / C
+        z = -D / C;
+
+        std::cout << "Точка перетину з осью X: (" << x << ", 0, 0)" << std::endl;
+        std::cout << "Точка перетину з осью Y: (0, " << y << ", 0)" << std::endl;
+        std::cout << "Точка перетину з осью Z: (0, 0, " << z << ")" << std::endl;
+    }
+
+    // Метод для перевірки належності точки площині
+    bool isPointOnPlane(double x, double y, double z) {
+        return (A * x + B * y + C * z + D) == 0;
+    }
+};
+
+int main() {
+    double a, b, c, d;
+
+    std::cout << "Введіть коефіцієнти A, B, C, D для рівняння площини (Ax + By + Cz + D = 0): ";
+    std::cin >> a >> b >> c >> d;
+
+    PlaneEquation plane(a, b, c, d);
+    plane.display();
+
+    plane.findIntersectionWithAxes();
+
+    double x, y, z;
+    std::cout << "Введіть координати точки (x, y, z) для перевірки: ";
+    std::cin >> x >> y >> z;
+
+    if (plane.isPointOnPlane(x, y, z)) {
+        std::cout << "Точка належить площині." << std::endl;
+    }
+    else {
+        std::cout << "Точка не належить площині." << std::endl;
+    }
+
+    return 0;
+}
